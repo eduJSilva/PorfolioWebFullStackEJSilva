63bb63c9a13ca58bd96faf506f52d41cb2b32e25
Eduardo J Silva <silvaeduardojavier@hotmail.com>
2022-04-30 21:56:28 -0300

    BackEnd SpringBoot Porfolio Terminado

---
+++ Back-End/Back-End-SpringBoot/src/main/java/com/portfolio/EduSilva/EduSilvaApplication.java
@@ -0,0 +1,21 @@
+package com.portfolio.EduSilva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+@SpringBootApplication
+
+@EntityScan(basePackageClasses = {
+    EduSilvaApplication.class,
+    Jsr310JpaConverters.class
+})
+public class EduSilvaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduSilvaApplication.class, args);
+    }
+
+}
